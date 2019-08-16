9c134364c4d7f521d593b4a0d1f9e3210c17e04e
Nina-wang907 <53446002+Nina-wang907@users.noreply.github.com>
2019-08-16 02:32:38 -0400

    update2

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
+// document.getElementById("items1").addEventListener("mouseover", change1);
+// document.getElementById("items1").addEventListener("mouseout", back1);
+
+// function change1(){
+// 	document.getElementById("items1").innerHTML='<a href="US.html"><img src="materials/usc.png" alt="US"width="301"height="179"></a>';
+// }
+
+// function back1(){
+// 	document.getElementById("items1").innerHTML='<a href="US.html"><img src="materials/US.png" alt="US"width="301"height="179"></a>';
+// }
+
+
 
